59b97b0a24b841ee467ef1f96ef450a77065ec2b
O.Yann André <115635958+Perito009@users.noreply.github.com>
2025-12-01 12:28:35 +0000

    Test/Feat: Ajout de rm le repertoir energetic et test pipeline dans sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -8,3 +8,8 @@ cd /workspaces/Projet_Collecte_Donn-es_IA
 
 # Exécuter le script Python et rediriger les logs
 python3 main_pipeline.py >> /workspaces/Projet_Collecte_Donn-es_IA/logs/pipeline.log 2>&1
+
+# Supprimer le dossier energetic_pipeline dans Extract après l'exécution du pipeline
+rm -rf Extract/energetic_pipeline
+
+echo "Le dossier Extract/energetic_pipeline a été supprimé."
