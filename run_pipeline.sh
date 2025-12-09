c6310536a65b7c9aad9472235fce8f2d56dedca1
O.Yann André <115635958+Perito009@users.noreply.github.com>
2025-12-09 12:58:51 +0000

    Fix: correction du fichier .sh pour qu'il suprimer if exist le fichier dashboard et wind_speed

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -7,14 +7,14 @@ mkdir -p /workspaces/Projet_Collecte_Donn-es_IA/logs
 
 # Se déplacer dans le répertoire du projet
 cd /workspaces/Projet_Collecte_Donn-es_IA
-
 echo "Répertoire du projet : $(pwd)"
-
+echo "Suppression du fichier dashboard.html et wind_speed_hist.png s'il existe..."
 # Supprimer le fichier dashboard.html s'il existe
 rm -rf /workspaces/Projet_Collecte_Donn-es_IA/dashboard.html
+echo "Suppression du fichier wind_speed_hist.png..."
+rm -rf /workspaces/Projet_Collecte_Donn-es_IA/wind_speed_hist.png
 
 echo "Exécution du script main_pipeline.py..."
-
 # Exécuter le script Python et rediriger les logs
 python3 main_pipeline.py >> /workspaces/Projet_Collecte_Donn-es_IA/logs/pipeline.log 2>&1
 
@@ -23,10 +23,11 @@ echo "Exécution du script dashboard_generator.py..."
 # Exécuter le script dashboard_generator.py et rediriger les logs
 python3 ./Graphics/dashboard_generator.py >> /workspaces/Projet_Collecte_Donn-es_IA/logs/pipeline.log 2>&1
 
+echo "Suppression du dossier Extract/energitic_pipeline..."
 # Supprimer le dossier energitic_pipeline dans Extract après l'exécution du pipeline
 rm -rf /workspaces/Projet_Collecte_Donn-es_IA/Extract/energitic_pipeline
 
-echo "Le dossier Extract/energitic_pipeline a été supprimé."
 
+echo "Lancement du serveur HTTP..."
 # Lancer un serveur HTTP pour servir le contenu du répertoire Graphics
 python3 -m http.server
